a5c1e2f634299b16e54ad61b13faff8479fdd8cc
TaiKie <tentingsanook@gmail.com>
2024-04-22 13:02:38 +0700

    Code Set 1

---
+++ Code/CalculateGrade.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class CalculateGrade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        if ((score >= 80) & (score <= 100)) {
+            System.out.println("A");
+        }
+        else if ((score >= 70) & (score <= 79)) {
+            System.out.println("B");
+        }
+        else if ((score >= 60) & (score <= 69)) {
+            System.out.println("C");
+        }
+        else if ((score >= 50) & (score <= 59)) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}
